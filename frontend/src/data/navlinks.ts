1e758a361f9fe6c6fa994d831f8ac7c1c0166607
Rakib-dhali~ <rakibdhali343@gmail.com>
2026-04-12 12:34:58 +0600

    navbar

--- frontend/src/data/navlinks.ts
+++ frontend/src/data/navlinks.ts
@@ -2,7 +2,8 @@ import type { INavLink } from "../types";
 
 export const navlinks: INavLink[] = [
     { name: "Home", href: "/" },
-    { name: "Features", href: "#features" },
-    { name: "Testimonials", href: "#testimonials" },
+    { name: "Generate", href: "/generate" },
+    { name: "My Generations", href: "/my-generations" },
     { name: "Pricing", href: "#pricing" },
+    { name: "Login", href: "/login" }
 ];
